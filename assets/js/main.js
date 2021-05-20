8eaa4eaad43c29a9b126ad198a34a98b1c77a2ec
Babajide Tomoshegbo <babajidetomoshegbo@yahoo.com>
2021-05-20 11:27:42 +0100

    ssdsa

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,5 +248,17 @@
       mirror: false
     })
   });
-
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+      
+      $(this).addClass('transition');
+    }, function(){
+          
+      $(this).removeClass('transition');
+    });
+      
 })()
